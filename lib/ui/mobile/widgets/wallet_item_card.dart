14b859eca38037305f6b4c1d944194047a9b5b48
Rosl9kov <rosliakov.tolik@gmail.com>
2023-09-02 11:18:13 +0300

    transaction page

--- lib/ui/mobile/widgets/wallet_item_card.dart
+++ lib/ui/mobile/widgets/wallet_item_card.dart
@@ -5,46 +5,47 @@ import 'package:flutter/material.dart';
 
 class WalletItemCard extends StatelessWidget {
   final Wallet wallet;
-  const WalletItemCard({super.key, required this.wallet});
+  final List<double> size;
+  WalletItemCard({super.key, required this.wallet, required this.size});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10.0),
-      height: 168,
-      width: 260,
+      margin: EdgeInsets.all(size[0]),
+      height: size[1],
+      width: size[2],
       decoration: BoxDecoration(
         color: context.colors.mainElement,
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(size[3]),
       ),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+        padding: EdgeInsets.symmetric(horizontal: size[4], vertical: size[5]),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               wallet.title,
               style: AppTextStyle.mainWalletTitleText.copyWith(
-                fontSize: 24.0,
+                fontSize: size[6],
                 fontWeight: FontWeight.w700,
               ),
             ),
-            const SizedBox(height: 26),
+            SizedBox(height: size[7]),
             Text(
               wallet.description,
               style: AppTextStyle.mainWalletDescriptionText.copyWith(
-                fontSize: 16.0,
+                fontSize: size[8],
                 fontWeight: FontWeight.w400,
               ),
               overflow: TextOverflow.ellipsis,
             ),
-            const SizedBox(height: 4),
+            SizedBox(height: size[9]),
             Row(
               children: [
                 Text(
-                  '${wallet.balance} ${wallet.currency}',
+                  '${wallet.balance} ₽',
                   style: AppTextStyle.mainWalletBalanceText.copyWith(
-                    fontSize: 20.0,
+                    fontSize: size[10],
                     fontWeight: FontWeight.w500,
                   ),
                 ),
